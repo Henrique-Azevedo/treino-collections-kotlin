b30e99b56ab3d30d651d5b0fd1932a4c22f4c366
Henrique-Azevedo <henrique.azevedo.pro@gmail.com>
2022-07-02 17:22:27 -0300

    Initial commit - examples of using collections

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayOf.kt
@@ -0,0 +1,23 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val nomes = Array(3) { "" }
+    nomes[0] = "Maria"
+    nomes[1] = "Zaza"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+    println("---------------")
+    val nomes2 = arrayOf("Maria", "Zaza", "Pedro")
+
+    nomes2.sort()
+    nomes2.forEach {
+    println(it)
+    }
+}
+
+
+
+
